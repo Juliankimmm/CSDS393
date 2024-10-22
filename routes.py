ed8c13a44fa177cd0c482000ff9c2c35b6132d00
Juliankimmm <jkswimtkd@gmail.com>
2024-10-22 12:35:11 -0400

    small change

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Flask
 from models import db, User, WorkoutLog, Group, Post
 
 main = Blueprint('main', __name__)
+app = Flask(__name__)
 
 @main.route('/')
 def index():
@@ -26,7 +28,7 @@ def profile(user_id):
     workout_logs = WorkoutLog.query.filter_by(user_id=user_id).all()
     return render_template('profile.html', user=user, workout_logs=workout_logs)
 
-@main.route('/workout_log', methods=['GET', 'POST'])
+@app.route('/workout_log', methods=['GET', 'POST'])
 def workout_log():
     if request.method == 'POST':
         user_id = request.form['user_id']
@@ -34,10 +36,20 @@ def workout_log():
         reps = request.form['reps']
         weight = request.form['weight']
         rpe = request.form['rpe']
-        workout_log = WorkoutLog(user_id=user_id, exercise=exercise, reps=reps, weight=weight, rpe=rpe)
+
+        # Create a new instance of WorkoutLog
+        workout_log = WorkoutLog(user_id=user_id)
+        workout_log.exercise = exercise
+        workout_log.reps = reps
+        workout_log.weight = weight
+        workout_log.rpe = rpe
+
+        # Add the new workout log entry to the session and commit
         db.session.add(workout_log)
         db.session.commit()
-        return redirect(url_for('main.profile', user_id=user_id))
+
+        return redirect('/workout_log')  # Redirect to show the updated log
+
     return render_template('workout_log.html')
 
 @main.route('/group', methods=['GET', 'POST'])
@@ -51,3 +63,6 @@ def group():
         return redirect(url_for('main.group'))
     groups = Group.query.all()
     return render_template('group.html', groups=groups)
+
+if __name__ == "__main__":
+    app.run(debug=True)
